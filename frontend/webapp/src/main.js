8a135b70a4596009e4973a5ceb8a1b657de62d87
贲冠然 <1755742107@qq.com>
2022-07-04 12:47:54 +0800

    更改sass版本

--- frontend/webapp/src/main.js
+++ frontend/webapp/src/main.js
@@ -8,5 +8,5 @@ import '@/assets/global.css' // 全局样式
 import SocketIO from './plugins/io' // 使用socketio
 
 createApp(App).use(store).use(router).use(ElementPlus).use(SocketIO, {
-    connection: 'ws://localhost:3001',
+    connection: 'ws://dgapi.aran.cc:3001/',
 }).mount('#app')
